dc7dc1cf89c3a9352795061f49805852858647cb
chocozono <chocozono@gmail.com>
2016-04-15 13:58:23 +0900

    Add write() method

--- client.go
+++ client.go
@@ -24,3 +24,12 @@ func (c *client) read() {
 	}
 	c.socket.Close()
 }
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
