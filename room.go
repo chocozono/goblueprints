fbf0a646ab23b7127b589852593d1cc985880e02
chocozono <chocozono@gmail.com>
2016-04-15 14:55:42 +0900

    Add some room members

--- room.go
+++ room.go
@@ -3,4 +3,13 @@ package main
 type room struct {
 	//forward represents the channel which has some messages for transporting to another clients
 	foward chan []byte
+
+	//join represents the channel for the client who wanna join to chat room
+	join chan *client
+
+	//leave represents the channel for the client who wanna leave from chat room
+	leave chan *client
+
+	//clients holds *client
+	clients map[*client]bool
 }
