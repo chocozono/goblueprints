7ba89674978b1c024a2b144a30f5c208970f2d21
chocozono <chocozono@gmail.com>
2016-04-17 17:59:03 +0900

    Add trace log messages

--- room.go
+++ room.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/chocozono/trace"
 	"github.com/gorilla/websocket"
 )
 
@@ -21,6 +22,9 @@ type room struct {
 	// treating this map exepts using channel is to be deprecated because
 	// there is a possibility that some goroutines change at the same time
 	clients map[*client]bool
+
+	//tracer receves operation log from chat room
+	tracer trace.Tracer
 }
 
 //new room generates and returns the chat room
@@ -39,20 +43,25 @@ func (room *room) run() {
 		case client := <-room.join:
 			// join
 			room.clients[client] = true
+			room.tracer.Trace("Joined new client")
 		case client := <-room.leave:
 			// leave
 			delete(room.clients, client)
 			close(client.send)
+			room.tracer.Trace("Leaved the client")
 		case msg := <-room.foward:
+			room.tracer.Trace("Recived message: ", string(msg))
 			// send messages to all clients
 			for client := range room.clients {
 				select {
 				case client.send <- msg:
 					// send a message
+					room.tracer.Trace(" -- Success -- Send message")
 				default:
 					// if failed sending message
 					delete(room.clients, client)
 					close(client.send)
+					room.tracer.Trace(" -- Fail -- Send message ")
 				}
 			}
 		}
